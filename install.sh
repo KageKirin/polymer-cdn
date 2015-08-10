317fe36116791b947182ab711759b92958d6ae72
Christian Helmich <christian.helmich@dena.com>
2015-08-10 11:44:59 +0900

    for some reason, this version installs all elements without issue

--- install.sh
+++ install.sh
@@ -2,36 +2,61 @@
 #npm install -g bower grunt
 
 #bower init
-bower install --save Polymer/polymer#^1.0.0
-bower install --save webcomponentsjs ## comes with polymer
+bower install --save \
+Polymer/polymer#^1.0.0 \
+PolymerElements/iron-elements#^1.0.0 \
+PolymerElements/paper-elements#^1.0.0 \
+PolymerElements/gold-elements#^1.0.0 \
+GoogleWebComponents/firebase-element#^1.0.0 \
+GoogleWebComponents/google-analytics#^1.0.0 \
+GoogleWebComponents/google-apis#^1.0.0 \
+GoogleWebComponents/google-calendar#^1.0.0 \
+GoogleWebComponents/google-castable-video#^1.0.0 \
+GoogleWebComponents/google-chart#^1.0.0 \
+GoogleWebComponents/google-feeds#^1.0.0 \
+GoogleWebComponents/google-hangout-button#^1.0.0 \
+GoogleWebComponents/google-map#^1.0.0 \
+GoogleWebComponents/google-sheets#^1.0.0 \
+GoogleWebComponents/google-signin#^1.0.0 \
+GoogleWebComponents/google-streetview-pano#^1.0.0 \
+GoogleWebComponents/google-url-shortener#^1.0.0 \
+GoogleWebComponents/google-youtube#^1.0.0 \
+GoogleWebComponents/google-youtube-upload#^1.0.0 \
+PolymerElements/neon-animation#^1.0.0 \
+PolymerElements/platinum-sw#^1.0.0 \
+PolymerElements/marked-element#^1.0.0
+
+
+##bower install --save webcomponentsjs ## comes with polymer
 
 # common elements: iron, paper, gold
-bower install --save PolymerElements/iron-elements#^1.0.0
-bower install --save PolymerElements/paper-elements#^1.0.0
-bower install --save PolymerElements/gold-elements#^1.0.0
+#bower install --save PolymerElements/iron-elements#^1.0.0
+#bower install --save PolymerElements/paper-elements#^1.0.0
+#bower install --save PolymerElements/gold-elements#^1.0.0
+
 
 # google web components
-bower install --save GoogleWebComponents/firebase-element
-bower install --save GoogleWebComponents/google-analytics
-bower install --save GoogleWebComponents/google-apis
-bower install --save GoogleWebComponents/google-calendar
-bower install --save GoogleWebComponents/google-castable-video
-bower install --save GoogleWebComponents/google-chart
-bower install --save GoogleWebComponents/google-feeds
-bower install --save GoogleWebComponents/google-hangout-button
-bower install --save GoogleWebComponents/google-map
-bower install --save GoogleWebComponents/google-sheets
-bower install --save GoogleWebComponents/google-signin
-bower install --save GoogleWebComponents/google-streetview-pano
-bower install --save GoogleWebComponents/google-url-shortener
-bower install --save GoogleWebComponents/google-youtube
-bower install --save GoogleWebComponents/google-youtube-upload
+#bower install --save GoogleWebComponents/firebase-element#^1.0.0
+#bower install --save GoogleWebComponents/google-analytics#^1.0.0
+#bower install --save GoogleWebComponents/google-apis#^1.0.0
+#bower install --save GoogleWebComponents/google-calendar#^1.0.0
+#bower install --save GoogleWebComponents/google-castable-video#^1.0.0
+#bower install --save GoogleWebComponents/google-chart#^1.0.0
+#bower install --save GoogleWebComponents/google-feeds#^1.0.0
+#bower install --save GoogleWebComponents/google-hangout-button#^1.0.0
+#bower install --save GoogleWebComponents/google-map#^1.0.0
+#bower install --save GoogleWebComponents/google-sheets#^1.0.0
+#bower install --save GoogleWebComponents/google-signin#^1.0.0
+#bower install --save GoogleWebComponents/google-streetview-pano#^1.0.0
+#bower install --save GoogleWebComponents/google-url-shortener#^1.0.0
+#bower install --save GoogleWebComponents/google-youtube#^1.0.0
+#bower install --save GoogleWebComponents/google-youtube-upload#^1.0.0
 
 # neon elements
-bower install --save PolymerElements/neon-animation
+#bower install --save PolymerElements/neon-animation#^1.0.0
 
 # platinum elements
-bower install --save PolymerElements/platinum-sw
+#bower install --save PolymerElements/platinum-sw#^1.0.0
 
 # molecules
-bower install --save PolymerElements/marked-element
+#bower install --save PolymerElements/marked-element#^1.0.0
